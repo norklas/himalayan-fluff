31032b0f248feddda6a3186e02351ede6e2c94c6
Nick Melanson <nickm890@gmail.com>
2022-03-27 13:20:26 -0400

    failed engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,28 @@
+// Engineer constructor
+const Engineer = require("../lib/Engineer");
+
+// Testing engineer object creation
+test("creates an Engineer object", () => {
+  const engineer = new Engineer("Nick", 60, "nickm890@gmail.com", "norklas");
+
+  expect(engineer.name).toEqual(expect.any(String));
+  expect(engineer.id).toEqual(expect.any(Number));
+  expect(engineer.email).toEqual(expect.any(String));
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+// Testing github from getGithub()
+test("gets Engineer github", () => {
+  const engineer = new Engineer("Nick", 60, "nickm890@gmail.com", "norklas");
+
+  expect(engineer.getGithub()).toEqual(
+    expect.stringContaining(engineer.github.toString())
+  );
+});
+
+// Testing role from getRole()
+test("get role of employee", () => {
+  const engineer = new Engineer("Nick", 60, "nickm890@gmail.com", "norklas");
+
+  expect(engineer.getRole()).toEqual("Engineer");
+});
