30d0eaaa76a83582d7e6efadfc5aa3ba200f1170
Nick Melanson <nickm890@gmail.com>
2022-03-27 13:50:13 -0400

    failed intern tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,28 @@
+// Importing Intern constructor
+const Intern = require("../lib/Intern");
+
+// Testing intern object creation
+test("creates an Intern object", () => {
+  const intern = new Intern("Nick", 60, "nickm890@gmail.com", "UConn");
+
+  expect(intern.name).toEqual(expect.any(String));
+  expect(intern.id).toEqual(expect.any(Number));
+  expect(intern.email).toEqual(expect.any(String));
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+// Testing school from getSchool()
+test("gets Intern school", () => {
+  const intern = new Intern("Nick", 60, "nickm890@gmail.com", "UConn");
+
+  expect(intern.getSchool()).toEqual(
+    expect.stringContaining(intern.school.toString())
+  );
+});
+
+// Testing role from getRole()
+test("gets role of employee", () => {
+  const intern = new Intern("Nick", 60, "nickm890@gmail.com", "UConn");
+
+  expect(inter.getRole()).toEqual("Intern");
+});
