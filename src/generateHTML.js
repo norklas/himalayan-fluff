07bed4e2c73ba0fe1ff7ec3f7aadd030372547dd
Nick Melanson <nickm890@gmail.com>
2022-03-28 15:26:53 -0400

    added google fonts, touched up bootstrap and css

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -121,6 +121,9 @@ const generateTeamPage = function (employeeCards) {
         <title>Team Profile</title>
         <script src="https://kit.fontawesome.com/88aea52a00.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
